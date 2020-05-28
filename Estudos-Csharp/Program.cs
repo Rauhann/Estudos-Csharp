178400fd95eb9b08df5b34d8caa926308610c26d
Rauhann Chaves <rauhann2711@gmail.com>
2020-05-28 15:34:02 -0300

    First commit

---
+++ Estudos-Csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Estudos_Csharp.Entities;
+using Estudos_Csharp.Entities.Enums;
+using System;
+
+namespace Estudos_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
